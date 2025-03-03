53bec1306ac810cc578b8d72ad8ea7596f8081fb
Jacob Christensen <jacobchristensen811@gmail.com>
2025-03-02 17:06:43 -0700

    added functionality for seperate list task organization

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const closeListMakeButton = document.getElementById("closeListMakeButton");
     const submitListNameButton = document.getElementById("submitListNameButton");
     const listNameInput = document.getElementById("listNameInput");
-    const deleteListButton = document.getElementById("deleteListButton");
 
     const taskContainer = document.getElementById("taskContainer");
     const addTaskButton = document.getElementById("addTaskButton");
@@ -14,86 +13,190 @@ document.addEventListener("DOMContentLoaded", function() {
     const closeTaskMakeButton = document.getElementById("closeTaskMakeButton");
     const submitTaskNameButton = document.getElementById("submitTaskNameButton");
     const taskNameInput = document.getElementById("taskNameInput");
-    const deleteTaskButton = document.getElementById("deleteTaskButton");
-});
+    const clearTaskButton = document.getElementById("clearTaskButton");
 
-addListButton.addEventListener("click", function() {
-    listMakeModal.classList.remove("hidden");
-});
+    let taskLists = []; // Stores all lists and their tasks
+    let activeList = null; // The currently selected list    
 
-closeListMakeButton.addEventListener("click", function () {
-    listMakeModal.classList.add("hidden");
-});
+    
 
-submitListNameButton.addEventListener("click", function () {
-    const name = listNameInput.value.trim();
-    if (name) {
-        listNameInput.value = "";
+    document.addEventListener("click", function(event) {
+        if (event.target.classList.contains("deleteListButton")) {
+            deleteList(event);
+        }
+        else if (event.target.classList.contains("listItem") || event.target.classList.contains("listItemName")) {
+            let listElement;
+            if(event.target.classList.contains("listItem")) {
+                listElement = event.target;
+            } else {
+                listElement = event.target.parentElement;
+            }
+            let index = listElement.dataset.index;
+            activeList = taskLists[index];
+            renderTasks();
+        }
+        else if (event.target.id === "addTaskButton") {
+            taskMakeModal.classList.remove("hidden");
+        }
+        else if (event.target.classList.contains("taskCheckBox")) {
+            checkTask(event.target);
+        }
+    });
+
+    addListButton.addEventListener("click", function() {
+        listMakeModal.classList.remove("hidden");
+    });
+
+    closeListMakeButton.addEventListener("click", function () {
         listMakeModal.classList.add("hidden");
-        addList(name);
-    } else {
-        alert("Please enter a name!");
-    }
-});
+    });
 
-function addList(name) {
-    let list = `<div class="flex px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded items-center justify-between">
-                    <span class="w-24 block break-words whitespace-normal">${name}</span>
-                    <button class="deleteListButton bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded ml-2">Delete List</button>
-                </div>`;
-    listContainer.insertAdjacentHTML("beforeend", list);
-}
+    submitListNameButton.addEventListener("click", function () {
+        const name = listNameInput.value.trim();
+        if (name) {
+            listNameInput.value = "";
+            listMakeModal.classList.add("hidden");
+            addList(name);
+        } else {
+            alert("Please enter a name!");
+        }
+    });
 
-function deleteList(event) {
-    event.target.parentElement.remove();
-}
+    submitTaskNameButton.addEventListener("click", function () {
+        const name = taskNameInput.value.trim();
+        if (name) {
+            taskNameInput.value = "";
+            taskMakeModal.classList.add("hidden");
+            addTask(name);
+        } else {
+            alert("Please enter a name!");
+        }
+    });
 
-document.addEventListener("click", function(event) {
-    if (event.target.classList.contains("deleteListButton")) {
-        deleteList(event);
-    }
-});
+    addTaskButton.addEventListener("click", function() {
+        taskMakeModal.classList.remove("hidden");
+    });
 
-addTaskButton.addEventListener("click", function() {
-    taskMakeModal.classList.remove("hidden");
-});
-
-closeTaskMakeButton.addEventListener("click", function () {
-    taskMakeModal.classList.add("hidden");
-});
-
-submitTaskNameButton.addEventListener("click", function () {
-    const name = taskNameInput.value.trim();
-    if (name) {
-        taskNameInput.value = "";
+    closeTaskMakeButton.addEventListener("click", function () {
         taskMakeModal.classList.add("hidden");
-        addTask(name);
-    } else {
-        alert("Please enter a name!");
+    });
+
+    clearTaskButton.addEventListener("click", function() {
+        clearCheckedTasks();
+    });
+    
+    document.addEventListener("click", function(event) {
+        if (event.target.classList.contains("deleteTaskButton")) {
+            deleteTask(event.target);
+        }
+    });
+
+
+
+    /*---------------------------
+    List Functions
+    ----------------------------*/
+
+    function renderLists() {
+        listContainer.innerHTML = "";
+        taskLists.forEach((list, index) => {
+            let listElement = `<div data-index="${index}" data-id="${list.id}" class="listItem flex px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded items-center justify-between">
+                        <span class="listItemName w-24 block break-words whitespace-normal">${list.name}</span>
+                        <button class="deleteListButton bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded ml-2">Delete List</button>
+                    </div>`;
+            listContainer.insertAdjacentHTML("beforeend", listElement);
+        });
     }
-});
 
-function addTask(name) {
-    let task = `<!-- Task -->
-                <div class="task flex items-center justify-between bg-gray-200 hover:bg-gray-300 p-3 rounded ml-[70px]">
-                    <div class="flex items-center">
-                        <input type="checkbox" class="w-5 h-5 mr-3">
-                        <span class="text-lg">${name}</span>
-                    </div>
-                    <div class="flex space-x-2 ml-2">
-                        <button class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded">Edit</button>
-                        <button class="deleteTaskButton bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded">Delete</button>
-                    </div>
-                </div>`;
-    taskContainer.insertAdjacentHTML("beforeend", task);
-}
-
-function deleteTask(event) {
-    event.target.closest(".task").remove();
-}
-
-document.addEventListener("click", function(event) {
-    if (event.target.classList.contains("deleteTaskButton")) {
-        deleteTask(event);
+    function addList(name) {
+        let newList = {
+            name: name,
+            id: crypto.randomUUID(),
+            tasks: [],
+        };
+        taskLists.push(newList);
+        activeList = newList;
+        renderLists();
     }
+
+    function deleteList(event) {
+        let listElement = event.target.parentElement;
+        let index = listElement.dataset.index;
+        let id = listElement.dataset.id;
+        listElement.remove();
+        taskLists.splice(index, 1);
+        if(id === activeList.id) {
+            activeList = taskLists[index - 1]
+        }
+        renderLists();
+    }
+
+    function setActiveList() {
+
+    }
+
+    /*---------------------------
+    Task Functions
+    ----------------------------*/
+
+    function renderTasks() {
+        taskContainer.innerHTML = "";
+        activeList.tasks.forEach((task, index) => {
+            let isChecked = "";
+            if(task.checked) {
+                isChecked = "checked";
+            }
+            let taskElement = `<!-- Task -->
+                    <div data-index="${index}" class="task flex items-center justify-between bg-gray-200 hover:bg-gray-300 p-3 rounded ml-[70px]">
+                        <div class="flex items-center">
+                            <input type="checkbox" ${isChecked} class="taskCheckBox w-5 h-5 mr-3">
+                            <span class="taskName text-lg">${task.name}</span>
+                        </div>
+                        <div class="flex space-x-2 ml-2">
+                            <button class="editTaskButton bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded">Edit</button>
+                            <button class="deleteTaskButton bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded">Delete</button>
+                        </div>
+                    </div>`;
+            taskContainer.insertAdjacentHTML("beforeend", taskElement);
+        });
+    }
+
+    function addTask(name) {
+        let newTask = {
+            name: name,
+            checked: false
+        };
+        activeList.tasks.push(newTask);
+        renderTasks();
+    }
+
+    function deleteTask(element) {
+        let taskElement = element.closest(".task");
+        let index = taskElement.dataset.index;
+        taskElement.remove();
+        activeList.tasks.splice(index, 1);
+        renderTasks();
+    }
+
+    function checkTask(element) {
+        let taskElement = element.closest(".task");
+        let index = taskElement.dataset.index;
+        activeList.tasks[index].checked = element.checked;
+    }
+
+    function clearCheckedTasks() {
+        let checkedTasks = taskContainer.querySelectorAll('input[type="checkbox"]:checked');
+        checkedTasks.forEach(function(checkbox) {
+            deleteTask(checkbox);
+        });
+    }
+
+    function editTask() {
+
+    }
+
+    editTaskButton.addEventListener("click", function() {
+
+    });
+
 });
